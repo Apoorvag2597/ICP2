4ce87a51b13ddd952eca4ac82a6181110a59e391
Apoorvag2597 <42699442+Apoorvag2597@users.noreply.github.com>
2018-09-01 20:36:00 -0500

    A
    A

--- ROCKPAPER/game.js
+++ ROCKPAPER/game.js
@@ -1,12 +1,15 @@
-function game() {
+function game() //Creating a function named game
+{
 
-    var number = prompt("Please enter your number");
-    var random = Math.floor((Math.random() * 10) + 1);
-    if (number == random) {
-        document.getElementById('output').innerHTML = 'Win';
+    var number = prompt("Please enter your number"); //entering a number by user
+    var random = Math.floor((Math.random() * 10) + 1); // A random number is generated in between 1 to 10
+    if (number == random) //Checking if both the numbers are equal or not
+    {
+        document.getElementById('output').innerHTML = 'Win'; //If both the numbers are equal, Output is given as Win
 
     }
-    else {
+    else //Loop is repeated until the output is given as win
+        {
         game();
     }
 }
